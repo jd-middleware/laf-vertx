2b2f448c489e3ec90b5dd73beaba440f05425153
hexiaofeng <hexiaofeng@jd.com>
2018-08-28 17:37:51 +0800

    修改测试用例

--- src/test/java/com/jd/laf/web/vertx/RoutingVerticleTest.java
+++ src/test/java/com/jd/laf/web/vertx/RoutingVerticleTest.java
@@ -2,11 +2,13 @@ package com.jd.laf.web.vertx;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,7 @@ import org.junit.runner.RunWith;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -42,17 +45,19 @@ public class RoutingVerticleTest {
 
     @Test
     public void testConfig() throws InterruptedException {
+        final AtomicReference<String> result = new AtomicReference<>();
         CountDownLatch latch = new CountDownLatch(1);
-        client.get(8080, "localhost", "/hello").send(a -> {
+        client.get(8080, "localhost", "/hello?echo=1234567").send(a -> {
             if (a.succeeded()) {
                 HttpResponse<Buffer> response = a.result();
-                System.out.println(response.bodyAsString());
+                result.set(Json.decodeValue(response.body(), String.class));
             } else {
                 logger.log(Level.SEVERE, "failed", a.cause());
             }
             latch.countDown();
         });
         latch.await();
+        Assert.assertEquals(result.get(), "1234567");
     }
 
 }
