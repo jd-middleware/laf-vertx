5a7aa38d1f101a18290eb9bc7b7ea37b84913777
hexiaofeng <hexiaofeng@jd.com>
2018-10-17 11:08:27 +0800

    增加监控配置

--- laf-vertx-springboot/src/main/java/org/unbrokendome/vertx/spring/boot/VertxAutoConfiguration.java
+++ laf-vertx-springboot/src/main/java/org/unbrokendome/vertx/spring/boot/VertxAutoConfiguration.java
@@ -1,5 +1,7 @@
 package org.unbrokendome.vertx.spring.boot;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.unbrokendome.vertx.spring.SpringVertx;
 import org.unbrokendome.vertx.spring.VerticleDeploymentConfiguration;
 import org.unbrokendome.vertx.spring.VertxConfiguration;
+import org.unbrokendome.vertx.spring.actuator.metrics.VertxMetricsConfiguration;
 
 
 @Configuration
@@ -25,6 +28,12 @@ public class VertxAutoConfiguration {
     @ConditionalOnProperty(prefix = "vertx", name = "auto-deploy-verticles", matchIfMissing = true)
     @Import(VerticleDeploymentConfiguration.class)
     public static class VerticleDeploymentAutoConfiguration {
+    }
+
+    @Configuration
+    @ConditionalOnClass(MeterRegistry.class)
+    @Import(VertxMetricsConfiguration.class)
+    public static class VertxMetricsAutoConfiguration {
     }
 
 }
