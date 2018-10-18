25db9ba269b31329177a5010460d64084df7ef9f
hexiaofeng <hexiaofeng@jd.com>
2018-10-18 09:08:47 +0800

    调整监控

--- laf-vertx-springboot/src/main/java/org/unbrokendome/vertx/spring/actuator/metrics/VertxMetricsConfiguration.java
+++ laf-vertx-springboot/src/main/java/org/unbrokendome/vertx/spring/actuator/metrics/VertxMetricsConfiguration.java
@@ -1,31 +1,21 @@
 package org.unbrokendome.vertx.spring.actuator.metrics;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 引入micrometer-registry-prometheus依赖后，会自动注册PrometheusMeterRegistry
+ */
 @Configuration
 @EnableConfigurationProperties(VertxMetricsProperties.class)
-@ConditionalOnClass(MeterRegistry.class)
 public class VertxMetricsConfiguration {
 
     @Bean
-    @ConditionalOnProperty(prefix = "vertx.metrics", name = "enabled", havingValue = "true")
-    @ConditionalOnMissingBean(MeterRegistry.class)
-    @ConditionalOnClass(PrometheusMeterRegistry.class)
-    public PrometheusMeterRegistry prometheusMeterRegistry() {
-        return new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "vertx.metrics", name = "enabled", havingValue = "true")
+    @ConditionalOnProperty(prefix = "vertx.metrics", name = "enabled", havingValue = "true", matchIfMissing = true)
     @ConditionalOnBean(MeterRegistry.class)
     public VertxActuatorMetrics vertxActuatorMetrics(MeterRegistry meterRegistry, VertxMetricsProperties metricsProperties) {
         return new VertxActuatorMetrics(meterRegistry, metricsProperties);
--- laf-vertx-web/src/main/java/com/jd/laf/web/vertx/handler/VertxMetricHandler.java
+++ laf-vertx-web/src/main/java/com/jd/laf/web/vertx/handler/VertxMetricHandler.java
@@ -3,20 +3,42 @@ package com.jd.laf.web.vertx.handler;
 import com.jd.laf.web.vertx.Environment;
 import com.jd.laf.web.vertx.EnvironmentAware;
 import com.jd.laf.web.vertx.RoutingHandler;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.core.instrument.Measurement;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.RoutingContext;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
 
 /**
  * 系统指标
  */
 public class VertxMetricHandler implements RoutingHandler, EnvironmentAware {
 
-    protected PrometheusMeterRegistry registry;
+    public final static String CONTENT_TYPE_004 = "text/plain; version=0.0.4; charset=utf-8";
+
+    protected MeterRegistry registry;
 
     @Override
     public void setup(final Vertx vertx, final Environment environment) throws Exception {
-        registry = environment.getObject("prometheusMeterRegistry", PrometheusMeterRegistry.class);
+        //尝试获取MeterRegistry
+        String name = environment.getString("vertx.metric.registry.name");
+        if (name != null && !name.isEmpty()) {
+            registry = environment.getObject(name, MeterRegistry.class);
+        } else {
+            registry = environment.getObject("prometheusMeterRegistry", MeterRegistry.class);
+            if (registry == null) {
+                registry = environment.getObject("influxMeterRegistry", MeterRegistry.class);
+            }
+            if (registry == null) {
+                registry = environment.getObject("jmxMeterRegistry", MeterRegistry.class);
+            }
+        }
     }
 
     @Override
@@ -27,8 +49,126 @@ public class VertxMetricHandler implements RoutingHandler, EnvironmentAware {
     @Override
     public void handle(final RoutingContext context) {
         if (registry != null) {
-            String response = registry.scrape();
-            context.response().end(response);
+            StringWriter writer = new StringWriter();
+            try {
+                for (Meter meter : registry.getMeters()) {
+                    write004(writer, meter);
+                }
+                context.response().putHeader("Content-Type", CONTENT_TYPE_004).end(writer.toString());
+            } catch (IOException e) {
+                context.response().setStatusCode(500).putHeader("Content-Type", CONTENT_TYPE_004).end(e.getMessage());
+            }
+
+        } else {
+            context.response().setStatusCode(500).putHeader("Content-Type", CONTENT_TYPE_004).end("MeterRegistry is not found.");
+        }
+
+    }
+
+    protected static void write004(final Writer writer, final Meter meter) throws IOException {
+        Meter.Id id = meter.getId();
+        writer.write("# HELP ");
+        writer.write(id.getName());
+        writer.write(' ');
+        writeEscapedHelp(writer, id.getDescription());
+        writer.write('\n');
+        writer.write("# TYPE ");
+        writer.write(id.getName());
+        writer.write(' ');
+        switch (id.getType()) {
+            case GAUGE:
+                writer.write("gauge");
+                break;
+            case COUNTER:
+                writer.write("counter");
+                break;
+            case DISTRIBUTION_SUMMARY:
+                writer.write("summary");
+                break;
+            case TIMER:
+                writer.write("histogram");
+                break;
+            case LONG_TASK_TIMER:
+                writer.write("histogram");
+                break;
+            default:
+                writer.write("untyped");
+                break;
+        }
+        writer.write('\n');
+        List<Tag> tags;
+        int i;
+        for (Measurement measurement : meter.measure()) {
+            writer.write(id.getName());
+            tags = id.getTags();
+            if (tags != null && !tags.isEmpty()) {
+                i = 0;
+                writer.write('{');
+                for (Tag tag : tags) {
+                    if (i++ > 0) {
+                        writer.write(',');
+                    }
+                    writer.write(tag.getKey());
+                    writer.write("=\"");
+                    writeEscapedLabelValue(writer, tag.getValue());
+                    writer.write("\"");
+                }
+                writer.write('}');
+            }
+            writer.write(' ');
+            writer.write(doubleToGoString(measurement.getValue()));
+            writer.write('\n');
+        }
+    }
+
+    protected static void writeEscapedHelp(final Writer writer, final String s) throws IOException {
+        if (s == null) {
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '\\':
+                    writer.append("\\\\");
+                    break;
+                case '\n':
+                    writer.append("\\n");
+                    break;
+                default:
+                    writer.append(c);
+            }
+        }
+    }
+
+    protected static void writeEscapedLabelValue(final Writer writer, final String s) throws IOException {
+        if (s == null) {
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '\\':
+                    writer.append("\\\\");
+                    break;
+                case '\"':
+                    writer.append("\\\"");
+                    break;
+                case '\n':
+                    writer.append("\\n");
+                    break;
+                default:
+                    writer.append(c);
+            }
+        }
+    }
+
+    protected static String doubleToGoString(final double d) {
+        if (d == 1.0D / 0.0) {
+            return "+Inf";
+        } else if (d == -1.0D / 0.0) {
+            return "-Inf";
+        } else {
+            return Double.isNaN(d) ? "NaN" : Double.toString(d);
         }
     }
 }
