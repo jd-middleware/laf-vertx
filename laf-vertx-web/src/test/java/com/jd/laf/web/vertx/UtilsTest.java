1dc52953a0ba684a7364694a978d59e9414f4b4e
hexiaofeng <hexiaofeng@jd.com>
2018-10-29 16:42:30 +0800

    性能测试

--- laf-vertx-web/src/test/java/com/jd/laf/web/vertx/UtilsTest.java
+++ laf-vertx-web/src/test/java/com/jd/laf/web/vertx/UtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class UtilsTest {
 
     public static final int count = 1000000;
+    public static final String URL = "/v1/namespace/hexiaofeng/config/test/profiles/test";
 
     @Test
     public void testNormalizePath() {
@@ -22,9 +23,30 @@ public class UtilsTest {
         path = Utils.normalizePath("www.jd.com/abcd/%2E./acd%41");
         System.out.println(path);
 
+        System.out.println("========================");
+
+        path = Utils.normalize("www.jd.com/abcd/../acd%41");
+        System.out.println(path);
+        path = Utils.normalize("www.jd.com/abcd/..");
+        System.out.println(path);
+        path = Utils.normalize("www.jd.com/abcd//");
+        System.out.println(path);
+        path = Utils.normalize("www.jd.com/abcd/../..");
+        System.out.println(path);
+        path = Utils.normalize("www.jd.com/abcd/../..a");
+        System.out.println(path);
+        path = Utils.normalizePath("www.jd.com/abcd/%2E./acd%41");
+        System.out.println(path);
+
         long time = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
-            Utils.normalizePath("/v1/namespace/hexiaofeng/config/test/profiles/test");
+            Utils.normalizePath(URL);
+        }
+        System.out.println("tps:" + count * 1000 / (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            Utils.normalize(URL);
         }
         System.out.println("tps:" + count * 1000 / (System.currentTimeMillis() - time));
 
