68187a93a2356283b17b9937f38854d0863664d8
hexiaofeng <hexiaofeng@jd.com>
2018-11-13 15:09:37 +0800

    merge 1.0.0

--- laf-vertx-springboot/src/main/java/org/unbrokendome/vertx/spring/SpringVertx.java
+++ laf-vertx-springboot/src/main/java/org/unbrokendome/vertx/spring/SpringVertx.java
@@ -51,7 +51,6 @@ public class SpringVertx implements SmartLifecycle, BeanFactoryAware {
                        int startupPhase, boolean autoStartup) {
         this.factory = factory;
         this.options = new VertxOptions(options);
-        this.options.setClusterHost(NetUtils.getLocalHost()) ;
         this.verticleRegistrations = new ArrayList<>(verticleRegistrations);
         this.listeners = new ArrayList<>(listeners);
         this.verticleFactoryPrefix = verticleFactoryPrefix;
