890755d1a9d5b4e1f2447e7390691b2f53c3df14
hexiaofeng <hexiaofeng@jd.com>
2019-01-16 16:47:44 +0800

    包装成Vertx对象

--- laf-vertx-web-springboot-starter/src/main/java/com/jd/laf/web/vertx/spring/SpringVertx.java
+++ laf-vertx-web-springboot-starter/src/main/java/com/jd/laf/web/vertx/spring/SpringVertx.java
@@ -1,8 +1,24 @@
 package com.jd.laf.web.vertx.spring;
 
+import io.netty.channel.EventLoopGroup;
 import io.vertx.core.*;
+import io.vertx.core.datagram.DatagramSocket;
+import io.vertx.core.datagram.DatagramSocketOptions;
+import io.vertx.core.dns.DnsClient;
+import io.vertx.core.dns.DnsClientOptions;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetClientOptions;
+import io.vertx.core.net.NetServer;
+import io.vertx.core.net.NetServerOptions;
+import io.vertx.core.shareddata.SharedData;
 import io.vertx.core.spi.VerticleFactory;
 import io.vertx.core.spi.VertxFactory;
 import org.springframework.beans.factory.BeanFactory;
@@ -12,16 +28,18 @@ import org.springframework.core.Ordered;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 
 /**
  * 在Spring的生命周期启动Vertx
  */
-public class SpringVertx implements SmartLifecycle, BeanFactoryAware {
+public class SpringVertx implements SmartLifecycle, BeanFactoryAware, Vertx {
 
     private final Logger logger = LoggerFactory.getLogger(SpringVertx.class);
 
@@ -46,17 +64,17 @@ public class SpringVertx implements SmartLifecycle, BeanFactoryAware {
     }
 
     @Override
-    public final boolean isAutoStartup() {
+    public boolean isAutoStartup() {
         return autoStartup;
     }
 
     @Override
-    public final int getPhase() {
+    public int getPhase() {
         return startupPhase;
     }
 
     @Override
-    public final boolean isRunning() {
+    public boolean isRunning() {
         return vertx != null;
     }
 
@@ -102,7 +120,7 @@ public class SpringVertx implements SmartLifecycle, BeanFactoryAware {
         }
 
         try {
-            this.vertx = readyFuture.join();
+            vertx = readyFuture.join();
             logger.info("success starting Vert.x");
         } catch (CompletionException ex) {
             logger.error(ex.getMessage(), ex);
@@ -237,4 +255,273 @@ public class SpringVertx implements SmartLifecycle, BeanFactoryAware {
         this.beanFactory = beanFactory;
     }
 
+    @Override
+    public Context getOrCreateContext() {
+        return vertx.getOrCreateContext();
+    }
+
+    @Override
+    public NetServer createNetServer(NetServerOptions options) {
+        return vertx.createNetServer(options);
+    }
+
+    @Override
+    public NetServer createNetServer() {
+        return vertx.createNetServer();
+    }
+
+    @Override
+    public NetClient createNetClient(NetClientOptions options) {
+        return vertx.createNetClient(options);
+    }
+
+    @Override
+    public NetClient createNetClient() {
+        return vertx.createNetClient();
+    }
+
+    @Override
+    public HttpServer createHttpServer(HttpServerOptions options) {
+        return vertx.createHttpServer(options);
+    }
+
+    @Override
+    public HttpServer createHttpServer() {
+        return vertx.createHttpServer();
+    }
+
+    @Override
+    public HttpClient createHttpClient(HttpClientOptions options) {
+        return vertx.createHttpClient(options);
+    }
+
+    @Override
+    public HttpClient createHttpClient() {
+        return vertx.createHttpClient();
+    }
+
+    @Override
+    public DatagramSocket createDatagramSocket(DatagramSocketOptions options) {
+        return vertx.createDatagramSocket(options);
+    }
+
+    @Override
+    public DatagramSocket createDatagramSocket() {
+        return vertx.createDatagramSocket();
+    }
+
+    @Override
+    public FileSystem fileSystem() {
+        return vertx.fileSystem();
+    }
+
+    @Override
+    public EventBus eventBus() {
+        return vertx.eventBus();
+    }
+
+    @Override
+    public DnsClient createDnsClient(int port, String host) {
+        return vertx.createDnsClient(port, host);
+    }
+
+    @Override
+    public DnsClient createDnsClient() {
+        return vertx.createDnsClient();
+    }
+
+    @Override
+    public DnsClient createDnsClient(DnsClientOptions options) {
+        return vertx.createDnsClient(options);
+    }
+
+    @Override
+    public SharedData sharedData() {
+        return vertx.sharedData();
+    }
+
+    @Override
+    public long setTimer(long delay, Handler<Long> handler) {
+        return vertx.setTimer(delay, handler);
+    }
+
+    @Override
+    public TimeoutStream timerStream(long delay) {
+        return vertx.timerStream(delay);
+    }
+
+    @Override
+    public long setPeriodic(long delay, Handler<Long> handler) {
+        return vertx.setPeriodic(delay, handler);
+    }
+
+    @Override
+    public TimeoutStream periodicStream(long delay) {
+        return vertx.periodicStream(delay);
+    }
+
+    @Override
+    public boolean cancelTimer(long id) {
+        return vertx.cancelTimer(id);
+    }
+
+    @Override
+    public void runOnContext(Handler<Void> action) {
+        vertx.runOnContext(action);
+    }
+
+    @Override
+    public void close() {
+        stop();
+    }
+
+    @Override
+    public void close(Handler<AsyncResult<Void>> completionHandler) {
+        stop(() -> completionHandler.handle(Future.succeededFuture()));
+    }
+
+    @Override
+    public void deployVerticle(Verticle verticle) {
+        vertx.deployVerticle(verticle);
+    }
+
+    @Override
+    public void deployVerticle(Verticle verticle, Handler<AsyncResult<String>> completionHandler) {
+        vertx.deployVerticle(verticle, completionHandler);
+    }
+
+    @Override
+    public void deployVerticle(Verticle verticle, DeploymentOptions options) {
+        vertx.deployVerticle(verticle, options);
+    }
+
+    @Override
+    public void deployVerticle(Class<? extends Verticle> verticleClass, DeploymentOptions options) {
+        vertx.deployVerticle(verticleClass, options);
+    }
+
+    @Override
+    public void deployVerticle(Supplier<Verticle> verticleSupplier, DeploymentOptions options) {
+        vertx.deployVerticle(verticleSupplier, options);
+    }
+
+    @Override
+    public void deployVerticle(Verticle verticle, DeploymentOptions options, Handler<AsyncResult<String>> completionHandler) {
+        vertx.deployVerticle(verticle, options, completionHandler);
+    }
+
+    @Override
+    public void deployVerticle(Class<? extends Verticle> verticleClass, DeploymentOptions options, Handler<AsyncResult<String>> completionHandler) {
+        vertx.deployVerticle(verticleClass, options, completionHandler);
+    }
+
+    @Override
+    public void deployVerticle(Supplier<Verticle> verticleSupplier, DeploymentOptions options, Handler<AsyncResult<String>> completionHandler) {
+        vertx.deployVerticle(verticleSupplier, options, completionHandler);
+    }
+
+    @Override
+    public void deployVerticle(String name) {
+        vertx.deployVerticle(name);
+    }
+
+    @Override
+    public void deployVerticle(String name, Handler<AsyncResult<String>> completionHandler) {
+        vertx.deployVerticle(name, completionHandler);
+    }
+
+    @Override
+    public void deployVerticle(String name, DeploymentOptions options) {
+        vertx.deployVerticle(name, options);
+    }
+
+    @Override
+    public void deployVerticle(String name, DeploymentOptions options, Handler<AsyncResult<String>> completionHandler) {
+        vertx.deployVerticle(name, options, completionHandler);
+    }
+
+    @Override
+    public void undeploy(String deploymentID) {
+        vertx.undeploy(deploymentID);
+    }
+
+    @Override
+    public void undeploy(String deploymentID, Handler<AsyncResult<Void>> completionHandler) {
+        vertx.undeploy(deploymentID, completionHandler);
+    }
+
+    @Override
+    public Set<String> deploymentIDs() {
+        return vertx.deploymentIDs();
+    }
+
+    @Override
+    public void registerVerticleFactory(VerticleFactory factory) {
+        vertx.registerVerticleFactory(factory);
+    }
+
+    @Override
+    public void unregisterVerticleFactory(VerticleFactory factory) {
+        vertx.unregisterVerticleFactory(factory);
+    }
+
+    @Override
+    public Set<VerticleFactory> verticleFactories() {
+        return vertx.verticleFactories();
+    }
+
+    @Override
+    public boolean isClustered() {
+        return vertx.isClustered();
+    }
+
+    @Override
+    public <T> void executeBlocking(Handler<Future<T>> blockingCodeHandler, boolean ordered, Handler<AsyncResult<T>> asyncResultHandler) {
+        vertx.executeBlocking(blockingCodeHandler, ordered, asyncResultHandler);
+    }
+
+    @Override
+    public <T> void executeBlocking(Handler<Future<T>> blockingCodeHandler, Handler<AsyncResult<T>> asyncResultHandler) {
+        vertx.executeBlocking(blockingCodeHandler, asyncResultHandler);
+    }
+
+    @Override
+    public EventLoopGroup nettyEventLoopGroup() {
+        return vertx.nettyEventLoopGroup();
+    }
+
+    @Override
+    public WorkerExecutor createSharedWorkerExecutor(String name) {
+        return vertx.createSharedWorkerExecutor(name);
+    }
+
+    @Override
+    public WorkerExecutor createSharedWorkerExecutor(String name, int poolSize) {
+        return vertx.createSharedWorkerExecutor(name, poolSize);
+    }
+
+    @Override
+    public WorkerExecutor createSharedWorkerExecutor(String name, int poolSize, long maxExecuteTime) {
+        return vertx.createSharedWorkerExecutor(name, poolSize, maxExecuteTime);
+    }
+
+    @Override
+    public WorkerExecutor createSharedWorkerExecutor(String name, int poolSize, long maxExecuteTime, TimeUnit maxExecuteTimeUnit) {
+        return vertx.createSharedWorkerExecutor(name, poolSize, maxExecuteTime, maxExecuteTimeUnit);
+    }
+
+    @Override
+    public boolean isNativeTransportEnabled() {
+        return vertx.isNativeTransportEnabled();
+    }
+
+    @Override
+    public Vertx exceptionHandler(Handler<Throwable> handler) {
+        return vertx.exceptionHandler(handler);
+    }
+
+    @Override
+    public Handler<Throwable> exceptionHandler() {
+        return vertx.exceptionHandler();
+    }
 }
